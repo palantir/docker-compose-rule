b4f32c93097092e642917a19f528bcdb40d1a8b7
Felix de Souza <fdesouza@palantir.com>
2016-02-25 17:13:29 +0000

    Tidy up.

--- src/main/java/com/palantir/docker/compose/matchers/AvailablePortMatcher.java
+++ src/main/java/com/palantir/docker/compose/matchers/AvailablePortMatcher.java
@@ -1,7 +1,6 @@
 package com.palantir.docker.compose.matchers;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import org.hamcrest.Description;
@@ -10,10 +9,6 @@ import org.hamcrest.TypeSafeMatcher;
 import com.palantir.docker.compose.connection.DockerPort;
 
 public class AvailablePortMatcher extends TypeSafeMatcher<Collection<DockerPort>> {
-
-    public static AvailablePortMatcher areAvailable() {
-        return new AvailablePortMatcher();
-    }
 
     @Override
     public void describeTo(Description description) {
@@ -30,10 +25,14 @@ public class AvailablePortMatcher extends TypeSafeMatcher<Collection<DockerPort>
         mismatchDescription.appendValueList("These ports were unavailable:\n", "\n", ".", buildClosedPortsErrorMessage(unavailablePorts));
     }
 
-    private static List<String> buildClosedPortsErrorMessage(Collection<DockerPort> unavailablePorts) {
+    private static Collection<String> buildClosedPortsErrorMessage(Collection<DockerPort> unavailablePorts) {
         return unavailablePorts.stream()
                                .map(port -> "For host with ip address: " + port.getIp() + " external port '" + port.getExternalPort() + "' mapped to internal port '" + port.getInternalPort() + "' was unavailable")
                                .collect(Collectors.toList());
+    }
+
+    public static AvailablePortMatcher areAvailable() {
+        return new AvailablePortMatcher();
     }
 
 }
