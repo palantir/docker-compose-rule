ed819dc600d0f8097d015df8fc88bd412b6d61b0
Joe Lea <joeleapublic@gmail.com>
2016-03-04 15:50:28 +0000

    Shuffle around test methods

--- src/test/java/com/palantir/docker/compose/execution/DockerComposeLocationsShould.java
+++ src/test/java/com/palantir/docker/compose/execution/DockerComposeLocationsShould.java
@@ -15,25 +15,35 @@
  */
 package com.palantir.docker.compose.execution;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.File;
 import java.io.IOException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class DockerComposeLocationsShould {
+    private final String invalidFilePath = "file/that/does/not/exist";
+
     @Rule public TemporaryFolder folder = new TemporaryFolder();
 
+    String validFilePath;
+
+    @Before
+    public void setup() throws IOException {
+        validFilePath = folder.newFile("docker-compose.yml").getAbsolutePath();
+    }
+
     @Test public void
-    provide_a_docker_compose_location_if_it_exists() throws IOException {
-        File file = folder.newFile("docker-compose.yml");
-        DockerComposeLocations dockerComposeLocations = new DockerComposeLocations(file.getAbsolutePath());
+    provide_the_first_docker_compose_location_if_it_exists() throws IOException {
+        DockerComposeLocations dockerComposeLocations = new DockerComposeLocations(
+                validFilePath,
+                invalidFilePath);
 
         assertThat(dockerComposeLocations.preferredLocation().get(),
-                is(file.getAbsolutePath()));
+                is(validFilePath));
     }
 }
