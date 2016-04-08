b90ac6b6048270af71010d78045d650accca5187
Callum Rogers <callumr@palantir.com>
2016-04-08 14:50:14 +0100

    Add tests for ps

--- src/test/java/com/palantir/docker/compose/execution/RetryingDockerComposeShould.java
+++ src/test/java/com/palantir/docker/compose/execution/RetryingDockerComposeShould.java
@@ -1,5 +1,6 @@
 package com.palantir.docker.compose.execution;
 
+import com.palantir.docker.compose.connection.ContainerNames;
 import com.palantir.docker.compose.utils.MockitoMultiAnswer;
 import org.junit.Test;
 import org.mockito.invocation.InvocationOnMock;
@@ -14,12 +15,14 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 public class RetryingDockerComposeShould {
     private static final Object SUCCESS = null;
 
     private final DockerCompose dockerCompose = mock(DockerCompose.class);
     private final RetryingDockerCompose retryingDockerCompose = new RetryingDockerCompose(2, dockerCompose);
+    private final ContainerNames someContainerNames = new ContainerNames("hey");
 
     @Test
     public void not_retry_if_the_up_command_was_successful() throws IOException, InterruptedException {
@@ -48,7 +51,7 @@ public class RetryingDockerComposeShould {
     }
 
     @Test
-    public void throw_the_last_exception_when_failing_more_times_than_the_specified_attempts() throws IOException, InterruptedException {
+    public void throw_the_last_exception_when_up_fails_more_times_than_the_specified_attempts() throws IOException, InterruptedException {
         DockerComposeExecutionException finalException = new DockerComposeExecutionException();
         doAnswer(new MockitoMultiAnswer() {
             @Override
@@ -70,6 +73,60 @@ public class RetryingDockerComposeShould {
         }
 
         verify(dockerCompose, times(2)).up();
+        verifyNoMoreInteractions(dockerCompose);
+    }
+
+    @Test
+    public void not_retry_if_the_ps_command_was_successful_and_return_the_correct_container_names() throws IOException, InterruptedException {
+        when(dockerCompose.ps()).thenReturn(someContainerNames);
+
+        assertThat(retryingDockerCompose.ps(), is(someContainerNames));
+        verify(dockerCompose).ps();
+        verifyNoMoreInteractions(dockerCompose);
+    }
+
+    @Test
+    public void retry_ps_if_the_command_failed_once_and_return_the_last_container_names() throws IOException, InterruptedException {
+        when(dockerCompose.ps()).thenAnswer(new MockitoMultiAnswer() {
+            @Override
+            protected Object firstCall(InvocationOnMock invocation) throws Exception {
+                throw new DockerComposeExecutionException();
+            }
+
+            @Override
+            protected Object secondCall(InvocationOnMock invocation) throws Exception {
+                return someContainerNames;
+            }
+        });
+
+        assertThat(retryingDockerCompose.ps(), is(someContainerNames));
+        verify(dockerCompose, times(2)).ps();
+        verifyNoMoreInteractions(dockerCompose);
+    }
+
+    @Test
+    public void throw_the_last_exception_when_ps_fails_more_times_than_the_specified_attempts() throws IOException, InterruptedException {
+        DockerComposeExecutionException finalException = new DockerComposeExecutionException();
+        when(dockerCompose.ps()).thenAnswer(new MockitoMultiAnswer() {
+            @Override
+            protected Object firstCall(InvocationOnMock invocation) throws Exception {
+                throw new DockerComposeExecutionException();
+            }
+
+            @Override
+            protected Object secondCall(InvocationOnMock invocation) throws Exception {
+                throw finalException;
+            }
+        });
+
+        try {
+            retryingDockerCompose.ps();
+            fail("Should have caught exception");
+        } catch (DockerComposeExecutionException actualException) {
+            assertThat(actualException, is(finalException));
+        }
+
+        verify(dockerCompose, times(2)).ps();
         verifyNoMoreInteractions(dockerCompose);
     }
 }
