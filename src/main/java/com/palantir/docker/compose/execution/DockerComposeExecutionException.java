f52c4b57849c954021f4461cb214856b532dba94
Callum Rogers <callumr@palantir.com>
2016-04-08 15:49:32 +0100

    Remove unused execption constructors

--- src/main/java/com/palantir/docker/compose/execution/DockerComposeExecutionException.java
+++ src/main/java/com/palantir/docker/compose/execution/DockerComposeExecutionException.java
@@ -7,12 +7,4 @@ public class DockerComposeExecutionException extends RuntimeException {
     public DockerComposeExecutionException(String message) {
         super(message);
     }
-
-    public DockerComposeExecutionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public DockerComposeExecutionException(Throwable cause) {
-        super(cause);
-    }
 }
