fa8ba8d087651c5c9e07d08e391c9c1cb78335a4
Dan Fox <dfox@palantir.com>
2016-10-02 19:12:57 +0100

    Logging for AggressiveShutdownStrategy

--- src/main/java/com/palantir/docker/compose/execution/AggressiveShutdownStrategy.java
+++ src/main/java/com/palantir/docker/compose/execution/AggressiveShutdownStrategy.java
@@ -6,6 +6,8 @@ package com.palantir.docker.compose.execution;
 
 import com.palantir.docker.compose.configuration.ShutdownStrategy;
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Shuts down containers as fast as possible, without giving them time to finish
@@ -13,9 +15,13 @@ import java.io.IOException;
  */
 public class AggressiveShutdownStrategy implements ShutdownStrategy {
 
+    private static final Logger log = LoggerFactory.getLogger(AggressiveShutdownStrategy.class);
+
     @Override
     public void shutdown(DockerCompose dockerCompose) throws IOException, InterruptedException {
+        log.info("Shutting down");
         dockerCompose.rm();
+        log.debug("Finished shutdown");
     }
 
 }
