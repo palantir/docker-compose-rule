a12076d96114ecb69b6ca6a0b7ff6a2beb9ccfbe
Huw Pryce <hpryce@palantir.com>
2017-03-07 14:20:31 +0000

    Reintroduce support for explicitly setting docker-compose location.

--- docker-compose-rule-core/src/main/java/com/palantir/docker/compose/execution/DockerCommandLocator.java
+++ docker-compose-rule-core/src/main/java/com/palantir/docker/compose/execution/DockerCommandLocator.java
@@ -19,50 +19,51 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
+import java.util.Optional;
 import java.util.regex.Pattern;
-
+import java.util.stream.Stream;
 import org.apache.commons.lang3.SystemUtils;
+import org.immutables.value.Value;
 
-public class DockerCommandLocator {
+@Value.Immutable
+public abstract class DockerCommandLocator {
     private static final Pattern PATH_SPLITTER = Pattern.compile(File.pathSeparator);
 
-    private final String command;
+    protected abstract String command();
 
-    public DockerCommandLocator(String command) {
-        this.command = command;
-    }
-
-    public String getLocation() {
-        // Get path variable, ignoring the case of its name
-        String path = getEnv().entrySet().stream()
-                .filter(e -> e.getKey().equalsIgnoreCase("path"))
-                .findFirst()
-                .map(Map.Entry::getValue)
-                .orElseThrow(() -> new IllegalStateException("Could not find path variable in env"));
-
-        // The filename is the same as the command, except on Windows where it ends with ".exe"
-        String filename = isWindows() ? command + ".exe" : command;
-
-        // Look through the directories in path for the given command which must exist and be executable
-        return PATH_SPLITTER.splitAsStream(path)
-                .map(p -> Paths.get(p, filename))
-                .filter(Files::exists)
-                .findFirst()
-                .map(Path::toString)
-                .orElseThrow(() -> new IllegalStateException("Could not find " + command + " in path"));
-    }
-
-    protected Map<String, String> getEnv() {
-        return System.getenv();
-    }
-
+    @Value.Default
     protected boolean isWindows() {
         return SystemUtils.IS_OS_WINDOWS;
     }
 
-    @Override
-    public String toString() {
-        return "DockerCommandLocator{command=" + command + "}";
+    @Value.Derived
+    protected String executableName() {
+        if (isWindows()) {
+            return command() + ".exe";
+        }
+        return command();
     }
+
+    @Value.Default
+    protected String path() {
+        String path = System.getenv("path");
+        if (path == null) {
+            throw new IllegalStateException("No path environment variable found");
+        }
+        return path;
+    }
+
+    protected abstract Optional<String> locationOverride();
+
+    public String getLocation() {
+        Stream<String> overrideLocation = locationOverride().map(l -> Stream.of(l)).orElse(Stream.empty());
+        Stream<String> pathLocations = PATH_SPLITTER.splitAsStream(path());
+        return Stream.concat(overrideLocation, pathLocations)
+                .map(p -> Paths.get(p, executableName()))
+                .filter(Files::exists)
+                .findFirst()
+                .map(Path::toString)
+                .orElseThrow(() -> new IllegalStateException("Could not find " + command() + " in path"));
+    }
+
 }
