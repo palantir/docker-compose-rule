d37af01c410f038d409e400079cd1e108aa39935
forozco <forozco@palantir.com>
2019-06-21 16:27:20 -0400

    isolate integration tests

--- docker-compose-rule-core/src/integrationTest/java/com/palantir/docker/compose/connection/ContainerIntegrationTests.java
+++ docker-compose-rule-core/src/integrationTest/java/com/palantir/docker/compose/connection/ContainerIntegrationTests.java
@@ -1,5 +1,17 @@
 /*
- * (c) Copyright 2016 Palantir Technologies Inc. All rights reserved.
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.palantir.docker.compose.connection;
@@ -15,12 +27,16 @@ import com.github.zafarkhaja.semver.Version;
 import com.jayway.awaitility.core.ConditionFactory;
 import com.palantir.docker.compose.configuration.DockerComposeFiles;
 import com.palantir.docker.compose.configuration.ProjectName;
+import com.palantir.docker.compose.connection.Container;
+import com.palantir.docker.compose.connection.DockerMachine;
+import com.palantir.docker.compose.connection.State;
 import com.palantir.docker.compose.execution.DefaultDockerCompose;
 import com.palantir.docker.compose.execution.Docker;
 import com.palantir.docker.compose.execution.DockerCompose;
 import com.palantir.docker.compose.execution.DockerExecutable;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.internal.matchers.GreaterOrEqual;
 
@@ -42,7 +58,7 @@ public class ContainerIntegrationTests {
 
         // The noHealthcheck service has no healthcheck specified; it should be immediately healthy
         Container container = new Container("noHealthcheck", docker, dockerCompose);
-        assertEquals(State.DOWN, container.state());
+        Assert.assertEquals(State.DOWN, container.state());
         container.up();
         assertEquals(State.HEALTHY, container.state());
         container.kill();
