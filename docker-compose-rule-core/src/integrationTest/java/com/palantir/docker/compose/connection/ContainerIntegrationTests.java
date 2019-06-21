cb6acbf0236bc17c2fa79991a669a1d494488f30
Felipe Orozco <ferozco@users.noreply.github.com>
2019-06-21 17:06:39 -0400

    Migrate to circle 2 (#279)
    * circle2
    
    * isolate integration tests
    
    * checkstyle

--- docker-compose-rule-core/src/integrationTest/java/com/palantir/docker/compose/connection/ContainerIntegrationTests.java
+++ docker-compose-rule-core/src/integrationTest/java/com/palantir/docker/compose/connection/ContainerIntegrationTests.java
@@ -1,5 +1,17 @@
 /*
- * (c) Copyright 2016 Palantir Technologies Inc. All rights reserved.
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.palantir.docker.compose.connection;
@@ -21,6 +33,7 @@ import com.palantir.docker.compose.execution.DockerCompose;
 import com.palantir.docker.compose.execution.DockerExecutable;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.internal.matchers.GreaterOrEqual;
 
@@ -42,7 +55,7 @@ public class ContainerIntegrationTests {
 
         // The noHealthcheck service has no healthcheck specified; it should be immediately healthy
         Container container = new Container("noHealthcheck", docker, dockerCompose);
-        assertEquals(State.DOWN, container.state());
+        Assert.assertEquals(State.DOWN, container.state());
         container.up();
         assertEquals(State.HEALTHY, container.state());
         container.kill();
