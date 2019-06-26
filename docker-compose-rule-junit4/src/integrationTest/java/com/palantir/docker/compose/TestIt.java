23c1ad8ad7fdc8e9a50c9da040d7e2c4d00969ae
Callum Rogers <callumr@palantir.com>
2019-06-26 18:00:18 -0400

    Demonstrate that we can get InstrumentedDockerComposeRule to work

---
+++ docker-compose-rule-junit4/src/integrationTest/java/com/palantir/docker/compose/TestIt.java
@@ -0,0 +1,37 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.docker.compose;
+
+import com.palantir.docker.compose.configuration.ShutdownStrategy;
+import com.palantir.docker.compose.logging.DoNothingLogCollector;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class TestIt {
+    @Rule
+    public DockerComposeRule dockerComposeRule = InstrumentedDockerComposeRule.builder()
+            .file("src/test/resources/shutdown-strategy.yaml")
+            .logCollector(new DoNothingLogCollector())
+            .retryAttempts(0)
+            .shutdownStrategy(ShutdownStrategy.AGGRESSIVE)
+            .build();
+
+    @Test
+    public void test() {
+
+    }
+}
