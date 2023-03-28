ecaea2adf6f10663d17a37e64fef1444f3752dc8
Michael Wintermeyer <michaelw@palantir.com>
2023-03-28 18:39:58 -0400

    Reverse DNS that makes sense

---
+++ docker-compose-junit-jupiter/src/integrationTest/java/com/palantir/docker/compose/ReverseDnsIntegrationTest.java
@@ -0,0 +1,57 @@
+/*
+ * (c) Copyright 2023 Palantir Technologies Inc. All rights reserved.&#10;&#10;Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);&#10;you may not use this file except in compliance with the License.&#10;You may obtain a copy of the License at&#10;&#10;    http://www.apache.org/licenses/LICENSE-2.0&#10;&#10;Unless required by applicable law or agreed to in writing, software&#10;distributed under the License is distributed on an &quot;AS IS&quot; BASIS,&#10;WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.&#10;See the License for the specific language governing permissions and&#10;limitations under the License.
+ */
+
+package com.palantir.docker.compose;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.palantir.docker.compose.configuration.DockerComposeFiles;
+import com.palantir.docker.compose.execution.DockerComposeExecArgument;
+import com.palantir.docker.compose.execution.DockerComposeExecOption;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+public class ReverseDnsIntegrationTest {
+    @RegisterExtension
+    public static final DockerComposeExtension docker = DockerComposeExtension.builder()
+            .files(DockerComposeFiles.from("src/integrationTest/resources/reverse-dns-compose.yaml"))
+            .build();
+
+    @Test
+    public void container_reverse_dns() throws IOException, InterruptedException {
+        System.out.println("Running first dig command");
+        String ipAddress = docker.dockerCompose()
+                .exec(
+                        DockerComposeExecOption.noOptions(),
+                        "db",
+                        DockerComposeExecArgument.arguments("dig", "+short", "db2.palantir.pt", "A"));
+        System.out.println(ipAddress);
+        System.out.println("Running second dig command");
+        String reverseHostName = docker.dockerCompose()
+                .exec(
+                        DockerComposeExecOption.noOptions(),
+                        "db",
+                        DockerComposeExecArgument.arguments("dig", "-x", ipAddress, "+short"));
+        System.out.println(reverseHostName);
+
+        System.out.println("Running first dig command on db2");
+        String ipAddress2 = docker.dockerCompose()
+                .exec(
+                        DockerComposeExecOption.noOptions(),
+                        "db2",
+                        DockerComposeExecArgument.arguments("dig", "+short", "db.palantir.pt", "A"));
+        System.out.println(ipAddress2);
+        System.out.println("Running second dig command on db2");
+        String reverseHostName2 = docker.dockerCompose()
+                .exec(
+                        DockerComposeExecOption.noOptions(),
+                        "db2",
+                        DockerComposeExecArgument.arguments("dig", "-x", ipAddress2, "+short"));
+        System.out.println(reverseHostName2);
+
+        assertThat(reverseHostName).matches("db2\\.palantir\\.pt\\..+_default\\.");
+        assertThat(reverseHostName2).matches("db\\.palantir\\.pt\\..+_default\\.");
+    }
+}
