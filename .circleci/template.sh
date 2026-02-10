4d0615114ca596295397b75b3692f4c8097ffd5b
Mohammed Daudali <mdaudali@palantir.com>
2026-02-10 15:06:02 +0000

    update circle

--- .circleci/template.sh
+++ .circleci/template.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export CIRCLECI_TEMPLATE=java-library-oss
 export DOCKER_TESTS=true
-export JAVA_11=true
-export JDK=11
+export JAVA_17=true
+export JDK=17
 export PRIMARY_BRANCH=master
